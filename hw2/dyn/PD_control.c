1ae652f41e500e1f9dc9990a7b1479f2bffc530b
anhkiet190999 <anhkiet190999@gmail.com>
2023-10-01 20:45:42 -0500

    add hw2

---
+++ hw2/dyn/PD_control.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int UTA_ID = 00000000000;
+
+double PD_control(theta, theta_dot, theta_ref, theta_dot_ref)
+double theta, theta_dot, theta_ref, theta_dot_ref;
+{
+  return(0.0);
+}
